e8b199a3fa0d487655cb07114e58833bec5b0903
unknown <poojithadarshan2131@gmail.com>
2023-11-08 08:56:57 +0530

    final

--- src/main/java/smvc/controller/StudentController.java
+++ src/main/java/smvc/controller/StudentController.java
@@ -1,9 +1,12 @@
 package smvc.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import smvc.dao.StudentDao;
@@ -11,24 +14,62 @@ import smvc.dto.Student;
 
 @Controller
 public class StudentController {
-	
+
 	@Autowired
 	private StudentDao studentDao;
 
 	@RequestMapping("/register")
 	public ModelAndView registerStudent() {
-		ModelAndView modelAndView=new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("student", new Student());
 		modelAndView.setViewName("register.jsp");
 		return modelAndView;
 	}
+
 	@RequestMapping("/save")
 	public ModelAndView saveStudent(@ModelAttribute Student student) {
-		ModelAndView modelAndView=new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView();
 		studentDao.saveStudent(student);
-		
+		List<Student> students = studentDao.getAllStudents();
+
+		modelAndView.addObject("list", students);
 		modelAndView.setViewName("display.jsp");
-		
+
 		return modelAndView;
 	}
+
+	@RequestMapping("/delete")
+	public ModelAndView deleteStudent(@RequestParam int id) {
+		ModelAndView modelAndView = new ModelAndView();
+		studentDao.deleteSTudentById(id);
+		
+		List<Student> students = studentDao.getAllStudents();
+
+		modelAndView.addObject("list", students);
+		modelAndView.setViewName("display.jsp");
+
+		return modelAndView;
+
+	}
+	@RequestMapping("/update")
+	public ModelAndView updateStudent(@RequestParam int id) {
+		ModelAndView modelAndView=new ModelAndView();
+		Student student=studentDao.findStudentById(id);
+		modelAndView.addObject("student", student);
+		modelAndView.setViewName("edit.jsp");
+		return modelAndView;	
+	}
+	@RequestMapping("/edit")
+	public ModelAndView editStudent(@ModelAttribute Student student) {
+		ModelAndView modelAndView=new ModelAndView();
+	    studentDao.updateStudent(student);
+	    
+	    List<Student> students = studentDao.getAllStudents();
+		modelAndView.addObject("list", students);
+		modelAndView.setViewName("display.jsp");
+
+		return modelAndView;
+	}
+	
+
 }
--- src/main/java/smvc/dao/StudentDao.java
+++ src/main/java/smvc/dao/StudentDao.java
@@ -1,7 +1,10 @@
 package smvc.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,17 +13,45 @@ import smvc.dto.Student;
 
 @Repository
 public class StudentDao {
-	
-	
+
 	@Autowired
 	private EntityManager entityManager;
 
 	public void saveStudent(Student student) {
-	EntityTransaction entityTransaction=entityManager.getTransaction();
-	entityTransaction.begin();
-	entityManager.persist(student);
-	entityTransaction.commit();
-		
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(student);
+		entityTransaction.commit();
+
+	}
+
+	public List<Student> getAllStudents() {
+		Query query = entityManager.createQuery("Select u from Student u");
+		return query.getResultList();
+	}
+
+	public void deleteSTudentById(int id) {
+		Student student = entityManager.find(Student.class, id);
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.remove(student);
+		entityTransaction.commit();
+
+	}
+
+	public Student findStudentById(int id) {
+		Student student = entityManager.find(Student.class, id);
+		return student;
+
+	}
+
+	public void updateStudent(Student student) {
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.merge(student);
+		entityTransaction.commit();
+
 	}
 
 }
